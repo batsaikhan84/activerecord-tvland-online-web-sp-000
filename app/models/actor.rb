3d7fb9ee2d35db3aed49a4e12f28c4873a9d61cf
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-26 23:11:17 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,6 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    self.characters.first.name
-    binding.pry
+    "#{self.characters.first.name} - #{self.shows.first.name}"
   end
 end
